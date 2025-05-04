3b2c4a9069b4d84f21dc1c7c7c4ea770b0609267
fabiha23 <fabiha.amatullah@gmail.com>
2025-05-04 14:13:41 +0600

    Setup: 1. Installed react router,tailwind,daisyui,react icons 2. Added Header, Navbar, and Asides 3. Made Asides sticky 4. Implemented dynamic routing 5. Displayed UI based on category selection 6. Added Marquee for Latest News

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -7,9 +7,9 @@ const Navbar = () => {
         <div className='flex items-center justify-between'>
             <div className='w-1/3'></div>
             <div className='text-accent flex justify-center w-1/3 gap-4 '>
-                <NavLink to='/'>Home</NavLink>
-                <NavLink to='/about'>About</NavLink>
-                <NavLink to='/career'>Career</NavLink>
+                <NavLink className='font-medium' to='/'>Home</NavLink>
+                <NavLink className='font-medium' to='/about'>About</NavLink>
+                <NavLink className='font-medium' to='/career'>Career</NavLink>
             </div>
             <div className='flex gap-2 justify-end w-1/3'>
                 <img width={34} src={user} alt="" />
